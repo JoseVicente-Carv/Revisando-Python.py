376704bd9fd17d77834de053ff963b58833defa6
José Vicente <160607761+JoseVicente-Carv@users.noreply.github.com>
2025-04-27 18:37:51 +0000

    Fiz uma copia da 2ª Atividade-Interface para entregar.

---
+++ Copia de Atividade 2-Interface_QR code.py
@@ -0,0 +1,53 @@
+""" Componente: José Vicente Carvalho dos Santos; Turmma 91165, Curso técnico em Desenvolvimento de Sistemas (3º Módulo)
+Materia: internet das Coisas    Data: 10/04/2025 """
+
+# importar biblioteca necessaria.
+import os
+import customtkinter as interface
+import qrcode
+from PIL import Image
+
+# definior funções necessarias.
+def gerarQRcode():
+    conteudo = textoDigitado.get() # Coletar texto digitado pelo usuario.
+
+    qrCodeGerado = qrcode.make(conteudo) # inserir conteudo digitado num QR Code.
+    qrCodeGerado.save("Seu novo QR Code.png") # Salvar novo QR Code como arquivo.
+
+os.system('cls || clear') # limpar tela do termial.
+
+# criar & configurar janela do aplicativo como variável.
+interface.set_appearance_mode('dark')
+
+janela = interface.CTk()
+janela.title("Interface de QR Code -José Vicente") # definir nome da janela.
+janela.geometry("600x450") # definir dimensões da janela em pixels.
+janela.resizable(True, True) # Permitir que o usuário mude as dimensões da janela.
+
+# Exibir titulo no topo da janela.
+interface.CTkLabel(janela,
+                   text=("Gerador de QR Code Personalizado"),
+                   font=("Roboto", 25)).pack(pady = 10)
+
+# exibir barra para usuario inserir texto/conteudo do codigo qr.
+textoDigitado = interface.CTkEntry(janela, 400, 40,
+                                   placeholder_text=("Digite uma URL."),
+                                   font=("caliber", 16))
+textoDigitado.place(x=20, y=65)
+
+# Exibir botão para gerar o Codigo QR.
+botaoGerar = interface.CTkButton (janela, 100, 40,
+                                  text=("Gerar"),
+                                  font=("arial", 16, "bold"),
+                                  command=gerarQRcode)
+botaoGerar.place(x=425, y=65) # ativar função e posicionar botão na janela.
+
+# exibir código QR gerado na janela.
+imagemExibida = interface.CTkImage(Image.open("Seu novo QR Code.png"),
+                                   Image.open("Seu novo QR Code.png"),
+                                   (500, 225))
+
+frameImagem = interface.CTkLabel(janela, text=(""), image=imagemExibida)
+frameImagem.place(x=20, y=80)
+
+janela.mainloop() # Ativar função da janela.
